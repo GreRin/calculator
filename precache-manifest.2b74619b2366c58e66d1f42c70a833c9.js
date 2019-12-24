5a47fd16f8889bdcf2c9dee2fb5dd6b1e18ee227
GreRin <rigor1988@gmail.com>
2019-12-24 11:34:30 +0300

    Updates

---
+++ precache-manifest.2b74619b2366c58e66d1f42c70a833c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17c72fbf04aa3236bf4ba6be27737cc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "af398fa136e82c526eb1",
+    "url": "/calculator/static/css/main.ea0eeb62.chunk.css"
+  },
+  {
+    "revision": "9346215d6bb72704b517",
+    "url": "/calculator/static/js/2.39c345e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.39c345e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af398fa136e82c526eb1",
+    "url": "/calculator/static/js/main.4ada6abe.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  }
+]);
